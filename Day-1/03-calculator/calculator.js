2921f1de73cad8bb3b2ec7ba0fcc69ca3b0c8158
tkmagesh <tkmagesh77@gmail.com>
2015-08-22 12:01:30 +0530

    calculator problem

---
+++ Day-1/03-calculator/calculator.js
@@ -0,0 +1,11 @@
+/*
+
+Create a calculator object that exhibits the following methods
+- add(x,y)
+- subtract(x,y)
+- multiply(x,y)
+- divide(x,y)
+
+use the calculator to perform all the operations on 100 and 200 and display the result
+
+*/
