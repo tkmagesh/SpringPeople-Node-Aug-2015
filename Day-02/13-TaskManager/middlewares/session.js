1d039d3618e53180d57c18916c6cbfc09e243288
tkmagesh <tkmagesh77@gmail.com>
2015-08-23 15:19:07 +0530

    Server Sent Events

--- Day-02/13-TaskManager/middlewares/session.js
+++ Day-02/13-TaskManager/middlewares/session.js
@@ -2,13 +2,25 @@ var Guid = require('guid');
 
 var sessionStore = {};
 
+setInterval(function(){
+    for(var sessionid in sessionStore){
+        var sessionObj = sessionStore[sessionid];
+        if ((new Date() - sessionObj.lastAccessedTime) > 60000)
+            delete sessionStore[sessionid];
+    }
+}, 5000)
+
 module.exports = function(req, res, next){
     var sessionid = req.cookies.sessionid;
     if (typeof sessionid === 'undefined'){
         sessionid = Guid.raw();
-        sessionStore[sessionid] = {};
+        sessionStore[sessionid] = {
+            lastAccessedTime : new Date(),
+            data : {}
+        };
         res.cookie("sessionid", sessionid);
     }
-    req.session = sessionStore[sessionid];
+    sessionStore[sessionid].lastAccessedTime = new Date();
+    req.session = sessionStore[sessionid].data;
     next();
 }
