759a468c80140c8e1517a7ae68570612e01a326b
tkmagesh <tkmagesh77@gmail.com>
2015-08-23 10:44:51 +0530

    AppServer - Refactoring - final

--- Day-02/11-AppServer/calculatorProcessor.js
+++ Day-02/11-AppServer/calculatorProcessor.js
@@ -3,27 +3,13 @@ var calculator = require('./calculator');
 
 
 module.exports = function(req, res, next){
-    if (req.url.pathname === '/calculator' && req.method === 'GET'){
-        var operation = req.url.query.operation,
-            n1 = parseInt(req.url.query.n1,10),
-            n2 = parseInt(req.url.query.n2,10);
+    if (req.url.pathname === '/calculator'){
+        var operation = req.field('operation'),
+            n1 = parseInt(req.field('n1'),10),
+            n2 = parseInt(req.field('n2'),10);
         var result = calculator[operation](n1,n2);
         res.write(result.toString());
         res.end();
-    } else if (req.url.pathname === '/calculator' && req.method === 'POST'){
-        var rawData = '';
-        req.on('data', function(chunk){
-            rawData += chunk;
-        });
-        req.on('end', function(){
-            var data = qs.parse(rawData);
-            var operation = data.operation,
-                n1 = parseInt(data.n1,10),
-                n2 = parseInt(data.n2,10);
-            var result = calculator[operation](n1,n2);
-            res.write(result.toString());
-            res.end();
-        });
     } else {
         next();
     }
--- Day-02/11-AppServer/dataParser.js
+++ Day-02/11-AppServer/dataParser.js
@@ -1,7 +1,27 @@
 var url = require('url');
+var qs = require('querystring');
 
 module.exports = function(req, res, next){
     req.url = req.url === '/' ? '/index.html' : req.url;
     req.url = url.parse(req.url, true);
-    next();
+    req.body = {};
+    req.field = function(key){
+        if (typeof req.url.query[key] !== 'undefined'){
+            return req.url.query[key];
+        } else {
+            return req.body[key];
+        }
+    }
+    if (req.method === 'POST'){
+        var rawData = '';
+        req.on('data', function(chunk){
+            rawData += chunk;
+        });
+        req.on('end', function(){
+            req.body = qs.parse(rawData);
+            next();
+        })
+    } else {
+        next();
+    }
 }
